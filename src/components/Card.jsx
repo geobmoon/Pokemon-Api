830bf0155489be5f4677d85d860654c451c8bb2b
geobmoon <georginabordoy@gmail.com>
2024-01-09 19:59:56 -0300

    Se agrega el proyecto

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import '../sass/Card.scss'
+
+const Card = ({name, img}) => {
+  return (
+    <div className="card">
+        <div className="card__circle"></div>
+        <p className="card__name">{name}</p>
+        
+        <img className="card__img" src={img} alt="pokemon img"></img>
+    </div>
+  )
+}
+
+export  {Card}
